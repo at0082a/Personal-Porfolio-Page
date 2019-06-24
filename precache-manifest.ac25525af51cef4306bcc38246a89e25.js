b2aab864a9947f3c9e3b4fc8de1e3600896c8f7a
alex <you@example.com>
2019-06-24 23:00:21 +0000

    Updates

---
+++ precache-manifest.ac25525af51cef4306bcc38246a89e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a98c2f1dae1748cd562430622e1d1a",
+    "url": "/Personal-Portfolio-Page/index.html"
+  },
+  {
+    "revision": "f30cc65a9ee35db79a71",
+    "url": "/Personal-Portfolio-Page/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "cbda3e44e8644695cd09",
+    "url": "/Personal-Portfolio-Page/static/css/main.41be21d0.chunk.css"
+  },
+  {
+    "revision": "f30cc65a9ee35db79a71",
+    "url": "/Personal-Portfolio-Page/static/js/2.4888009e.chunk.js"
+  },
+  {
+    "revision": "cbda3e44e8644695cd09",
+    "url": "/Personal-Portfolio-Page/static/js/main.c73641d2.chunk.js"
+  },
+  {
+    "revision": "4f044e424b1bec1bede2",
+    "url": "/Personal-Portfolio-Page/static/js/runtime~main.de6fadd0.js"
+  }
+]);
