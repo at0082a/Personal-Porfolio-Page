4b63ab58027ec1d68ab949b7fed4d36754862229
alex <you@example.com>
2019-06-25 02:08:59 +0000

    Updates

--- precache-manifest.d73e5aee310a8c10cf1585dffca3d2e3.js
+++ precache-manifest.d73e5aee310a8c10cf1585dffca3d2e3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "91a98c2f1dae1748cd562430622e1d1a",
+    "revision": "0a59c98fd51ee4e6f7307abe025bbfdf",
     "url": "/Personal-Portfolio-Page/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Personal-Portfolio-Page/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "cbda3e44e8644695cd09",
+    "revision": "538c32f32fc2e748ca6b",
     "url": "/Personal-Portfolio-Page/static/css/main.41be21d0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Personal-Portfolio-Page/static/js/2.4888009e.chunk.js"
   },
   {
-    "revision": "cbda3e44e8644695cd09",
-    "url": "/Personal-Portfolio-Page/static/js/main.c73641d2.chunk.js"
+    "revision": "538c32f32fc2e748ca6b",
+    "url": "/Personal-Portfolio-Page/static/js/main.b2cbf586.chunk.js"
   },
   {
     "revision": "4f044e424b1bec1bede2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Personal-Portfolio-Page/precache-manifest.ac25525af51cef4306bcc38246a89e25.js"
+  "/Personal-Portfolio-Page/precache-manifest.d73e5aee310a8c10cf1585dffca3d2e3.js"
 );
 
 self.addEventListener('message', (event) => {
